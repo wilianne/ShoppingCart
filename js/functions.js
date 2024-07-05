03abf8a65a05dbb4711692f94bd32a4cd40583db
Wilianne Paixão <williannequaresmapaixao@gmail.com>
2024-07-05 13:55:10 -0300

    Value

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,40 @@
-
-
-function Add(product){
-    const quantity = document.getElementById('qtd_'+product);
-    const value = document.getElementById('value_'+product);
-    const total = document.getElementById('total_'+product);
+function changeQuantity(product, action) {
+    const quantity = document.getElementById('qtd_' + product);
+    const value = document.getElementById('value_' + product);
+    const total = document.getElementById('total_' + product);
     
-    quantity.innerHTML++;
-    
-    const result = (quantity.innerHTML* value.innerHTML);
-
-    total.innerHTML = result;
-    
-}
-function Remove(product){
-    const quantity = document.getElementById('qtd_'+product);
-    const value = document.getElementById('value_'+product);
-    const total = document.getElementById('total_'+product);
-    
-    if(quantity.innerHTML > 1){
-        quantity.innerHTML--;
+    let currentQuantity = parseInt(quantity.innerHTML);
+    if (action == '-' && currentQuantity == 0) {
+        alert("Attention! The quantity cannot be less than 0.");
+    } else {
+        if (action == "+") {
+            currentQuantity++;
+        } else {
+            currentQuantity--;
+        }
+        quantity.innerHTML = currentQuantity;
+        
+        const valueNumber = numberOnly(value.innerHTML);
+        const totalValue = currentQuantity * valueNumber;
+        total.innerHTML = valueFormat(totalValue);
+        Sum();
     }
-    
-    
-    let result = (total.innerHTML - value.innerHTML);
-    
-    
-
-    total.innerHTML = result;
 }
 
+function Sum() {
+    let sum = 0;
+    
+    for (let i = 1; i < 4; i++) {
+        let number = numberOnly(document.getElementById('total_' + i).innerHTML);
+        sum += number;
+    }
+    document.getElementById('subtotal').innerHTML = valueFormat(sum);
+}
+
+function numberOnly(n) {
+    return parseFloat(n.replace(/[^\d]/g, ''));
+}
+
+function valueFormat(n) {
+    return 'R$ ' + n.toLocaleString('pt-BR');
+}
